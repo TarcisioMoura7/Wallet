8eadb6ac7cfcfa7ed0f44b23e88b449a26178a9a
Tarcisio Moura <bigtharcisiomoura@gmail.com>
2021-09-09 19:43:18 -0300

    Updates

---
+++ precache-manifest.07e8a714f71b188cc72c784a7ce3af9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108065efca9f37c802482a90b7f1cc98",
+    "url": "/Wallet/index.html"
+  },
+  {
+    "revision": "47201688ac9164e2e88a",
+    "url": "/Wallet/static/css/main.4277c184.chunk.css"
+  },
+  {
+    "revision": "301955e00449f55ac234",
+    "url": "/Wallet/static/js/2.41bc0864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wallet/static/js/2.41bc0864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47201688ac9164e2e88a",
+    "url": "/Wallet/static/js/main.dd68c6c6.chunk.js"
+  },
+  {
+    "revision": "3c8409916c29869110ed",
+    "url": "/Wallet/static/js/runtime-main.5015c633.js"
+  }
+]);
